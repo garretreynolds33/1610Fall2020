9d88361c9cd66acb964e064862a8d2c9019eb89a
Garret <10901561@my.uvu.edu>
2020-09-03 16:05:56 -0600

    Character Mover Funtion

--- Cyborg Shrimp/Assets/Scripts/CharacterMover.cs
+++ Cyborg Shrimp/Assets/Scripts/CharacterMover.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CharacterMover : MonoBehaviour
 {
+    public float speed = 3f, x, y, z;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,18 @@ public class CharacterMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        var vInput = speed * Input.GetAxis("Vertical") * Time.deltaTime;
+        var hInput = speed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        transform.Translate(x,y,z);
+    }
+
+    public void Up()
+    {
+        transform.Translate(0, speed, 0);
+    }
+    public void Down()
+    {
+        transform.Translate(0, -speed, 0);
     }
 }
+
