622d818e4609f26a395985bbea1883f70bdbe6da
Garret <10901561@my.uvu.edu>
2020-09-12 17:01:09 -0600

    Ammo Commit

---
+++ Cyborg Shrimp/Assets/Scripts/Ammo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour
+{
+    public int ammo;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ammo++;
+           print(ammo);
+    }
+}
