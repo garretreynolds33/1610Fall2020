9b6fd5182d88fc934cf3145398d40f4f29075353
Garret <10901561@my.uvu.edu>
2020-09-15 18:33:41 -0600

    If and Rotation Code

---
+++ Cyborg Shrimp/Assets/Scripts/ExtraPowerUp.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using UnityEngine;
+
+public class ExtraPowerUp : MonoBehaviour
+{
+    public int value = 10;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        value++;
+        print(value);
+    }
+} 
---
+++ Cyborg Shrimp/Assets/Scripts/RotationMover.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationMover : MonoBehaviour
+{
+  public float speed = 10f;
+  public int count;
+  public Vector3 rotationVector3;
+
+  public string password;
+
+  private void Update()
+  {
+   
+    if (password == "OU812")
+    {
+      print("Password Correct");
+    }
+    
+    rotationVector3.y = speed * Time.deltaTime;
+    transform.Rotate(rotationVector3);
+  }
+}
