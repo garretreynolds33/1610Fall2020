16aa17a1fc2c20c1f122f7eb7bc480fab9b5875f
Garret <10901561@my.uvu.edu>
2020-09-17 16:59:32 -0600

    Unity Events

--- Cyborg Shrimp/Assets/Scripts/PowerUp.cs
+++ Cyborg Shrimp/Assets/Scripts/PowerUp.cs
@@ -2,13 +2,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PowerUp : MonoBehaviour
 {
     public int score;
+    public UnityEvent PowerUpEvent;
     private void OnTriggerEnter(Collider other)
     {
         score++;
         print(score);
+        PowerUpEvent.Invoke();
+            
     }
 }
