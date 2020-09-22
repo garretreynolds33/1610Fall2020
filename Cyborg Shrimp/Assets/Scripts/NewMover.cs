75588c0e97fba79cd27ec9e306ea3f7f1dda8551
Garret <10901561@my.uvu.edu>
2020-09-22 15:55:06 -0600

    Sep 22 Class

---
+++ Cyborg Shrimp/Assets/Scripts/NewMover.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NewMover : MonoBehaviour
+{
+    public CharacterController controller;
+    private Vector3 moveDirection;
+    public float movespeed = 3f, gravity = -9.81f, jumpForce = 5f;
+    private float yDirection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var moveSpeedInput = movespeed * Input.GetAxis("Horizontal");
+        moveDirection.Set(moveSpeedInput, yDirection ,0);
+
+        yDirection += gravity * Time.deltaTime;
+        
+        if (controller.isGrounded && moveDirection.y < 0)
+        {
+            yDirection = -1f;
+        }
+        
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            yDirection = jumpForce;
+        }
+        
+        controller.Move(moveDirection*Time.deltaTime);
+    }
+}
