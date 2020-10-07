900f826ab9df1c00f1b85f28749d923aa5dd0f5a
Garret <10901561@my.uvu.edu>
2020-10-07 16:08:09 -0600

    Create with Code Oct 7th

---
+++ CwC Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool inOnGround = true;
+    public bool gameOver;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && inOnGround)
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            inOnGround = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            inOnGround = true;   
+        } else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+        }
+    }
+}
