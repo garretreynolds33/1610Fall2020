bbd662794e146ff45f82f1b6ab45cef35f2a4a5b
Garret <10901561@my.uvu.edu>
2020-10-18 14:54:17 -0600

    Class Recording

--- Cyborg Shrimp/Assets/Scriptable Objects/FloatData.cs
+++ Cyborg Shrimp/Assets/Scriptable Objects/FloatData.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu]
 public class FloatData : ScriptableObject
@@ -10,5 +11,20 @@ public class FloatData : ScriptableObject
  public void UpdateValue(float number)
  {
   value += number;
+ }
+
+ public void ReplaceValue(float number)
+ {
+  value = number;
+ }
+ 
+ public void DisplayImage(Image img)
+ {
+  img.fillAmount = value;
+ }
+
+ public void DisplayNumber(Text txt)
+ {
+  txt.text = value.ToString();
  }
 }
---
+++ Cyborg Shrimp/Assets/Scriptable Objects/StringData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu]
+public class StringData : ScriptableObject
+{
+   public string value;
+
+   public void DisplayText(Text txt)
+   {
+      txt.text = value;
+   }
+}
