af1f5432295c66ed9ca060eacd6668d1ba7b4da7
Garret <10901561@my.uvu.edu>
2020-10-23 18:08:17 -0600

    Create with Code Oct 23rd

---
+++ CwC Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+    }
+}
