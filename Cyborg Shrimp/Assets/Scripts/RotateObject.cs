bbb322441e31d84cb8fdf6ed818a7de7903df4f0
Garret <10901561@my.uvu.edu>
2020-11-03 15:45:46 -0700

    Class Nov 3rd

---
+++ Cyborg Shrimp/Assets/Scripts/RotateObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public float rotateSpeed = 35f;
+    public bool canRotate;
+
+    public void ChangeCanRotate(bool can)
+    {
+        canRotate = can;
+    }
+    
+    void Update()
+    {
+        if (canRotate)
+        {
+            var finalSpeed = rotateSpeed * Time.deltaTime;
+            transform.Rotate(0,0,finalSpeed);
+
+        }
+        
+    }
+}
