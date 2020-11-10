c60a31bc0e84c00cc1ac0f6f0439e655f50a7693
Garret <10901561@my.uvu.edu>
2020-11-10 14:26:49 -0700

    1st Class

--- Cyborg Shrimp/Assets/Scriptable Objects/FloatData.cs
+++ Cyborg Shrimp/Assets/Scriptable Objects/FloatData.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 [CreateAssetMenu]
 public class FloatData : ScriptableObject
 {
+ public UnityEvent onZeroEvent;
+ 
  public float value;
 
  public void UpdateValue(float number)
@@ -20,6 +23,16 @@ public class FloatData : ScriptableObject
  
  public void DisplayImage(Image img)
  {
+  if (value <= 0)
+  {
+   onZeroEvent.Invoke();
+  }
+
+  if (value >= 1)
+  {
+   value = 1;
+  }
+  
   img.fillAmount = value;
  }
 
