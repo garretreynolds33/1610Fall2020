934e2f7b83e788e18f900f7aa365b4df9d32d07a
Garret <10901561@my.uvu.edu>
2020-11-10 15:35:19 -0700

    2nd Class

---
+++ Cyborg Shrimp/Assets/Scriptable Objects/DefencesSwitch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class DefencesSwitch : ScriptableObject
+{
+    public enum Defences
+    {
+        Shield,
+        ExtraHealth,
+        Potions,
+        SuperShield,
+        Buffs,
+        Armor,
+    }
+
+    public Defences currentDefence;
+
+    public void RunCurrentDefence()
+    {
+        switch (currentDefence)
+        {
+            case Defences.Shield:
+                
+                break;
+            case Defences.ExtraHealth:
+                break;
+            case Defences.Potions:
+                break;
+            case Defences.SuperShield:
+                break;
+            case Defences.Buffs:
+                break;
+           case Defences.Armor:
+               break;
+        }
+    }
+}
