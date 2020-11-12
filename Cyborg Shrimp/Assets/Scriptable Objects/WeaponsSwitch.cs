d3e720c116fb6db62f2638be66a9e86f0abb7463
Garret <10901561@my.uvu.edu>
2020-11-11 22:45:50 -0700

    Weapon Switch

---
+++ Cyborg Shrimp/Assets/Scriptable Objects/WeaponsSwitch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class WeaponsSwitch : ScriptableObject
+{
+    public enum Weapons
+    {
+        Sword,
+        Spear,
+        Crossbow,
+        Bow,
+        Boomerang,
+    }
+
+    public Weapons currentWeapons;
+
+    public void RunCurrentDefence()
+    {
+        switch (currentWeapons)
+        {
+            case Weapons.Sword:
+                break;
+            case Weapons.Spear:
+                break;
+            case Weapons.Crossbow:
+                break;
+            case Weapons.Bow:
+                break;
+            case Weapons.Boomerang:
+                break;
+        }
+    }
+}
