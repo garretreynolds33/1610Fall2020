3a99d82511cf1f61e62671390ca994c484ab2ba4
Garret <10901561@my.uvu.edu>
2020-11-13 15:48:43 -0700

    Homework Nov 13th

--- Garret Reynolds - Personal Project/Assets/Scripts/CharacterMover.cs
+++ Garret Reynolds - Personal Project/Assets/Scripts/CharacterMover.cs
@@ -1,6 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class CharacterMover : MonoBehaviour
 {
-    
+    private CharacterController controller;
+    public float moveSpeed = 5f;
+
+    private Vector3 moveDirection;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        var movespeedinput = moveSpeed * Input.GetAxis("Vertical");
+        moveDirection.Set(0,movespeedinput,0);
+
+        var movement = moveDirection * Time.deltaTime;
+        controller.Move(movement);
+    }
 }
