ee3b30975f69af25df5a74b93a0e79a33f5485cc
Garret <10901561@my.uvu.edu>
2020-11-16 19:11:53 -0700

    HW Nov 16th

--- Garret Reynolds - Personal Project/Assets/Scripts/CharacterMover.cs
+++ Garret Reynolds - Personal Project/Assets/Scripts/CharacterMover.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public class CharacterMover : MonoBehaviour
 {
     private CharacterController controller;
-    public float moveSpeed = 5f;
+    private float moveSpeed = 7f;
+
+    private float yRange = 5f;
 
     private Vector3 moveDirection;
 
@@ -15,10 +17,30 @@ public class CharacterMover : MonoBehaviour
 
     private void Update()
     {
-        var movespeedinput = moveSpeed * Input.GetAxis("Vertical");
-        moveDirection.Set(0,movespeedinput,0);
+        var verticalmovement = moveSpeed * Input.GetAxis("Vertical");
+        var horizontalmovement = moveSpeed * Input.GetAxis("Horizontal");
+        
+        moveDirection.Set(horizontalmovement,verticalmovement,0);
 
         var movement = moveDirection * Time.deltaTime;
         controller.Move(movement);
+
+        if (transform.position.y > yRange)
+        {
+            transform.position = new Vector3(transform.position.x, yRange, transform.position.z);
+            
+        } else if (transform.position.y < -yRange)
+        {
+            transform.position = new Vector3(transform.position.x, -yRange, transform.position.z);
+            
+        }
+
+        if (transform.position.x > -8)
+        {
+            transform.position = new Vector3(-8, transform.position.y, transform.position.z);
+        } else if (transform.position.x < -13)
+        {
+            transform.position = new Vector3(-13, transform.position.y, transform.position.z);
+        }
     }
 }
