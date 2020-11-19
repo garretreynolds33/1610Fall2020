a0e7a5afbfd3c6529a060feee0abfa87289ee1dd
Garret <10901561@my.uvu.edu>
2020-11-19 16:39:50 -0700

    Class Nov 19th

---
+++ Cyborg Shrimp/Assets/Scripts/CoroutineEvents.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroutineEvents : MonoBehaviour
+{
+    public UnityEvent startEvent, repeatEvent, endEvent;
+    public float holdTime, repeatHoldTime = 0.25f;
+    public int counter = 3;
+
+    public void RunCoroutine()
+    {
+        StartCoroutine(Coroutine());
+    }
+
+    private IEnumerator Coroutine()
+    {
+        startEvent.Invoke();
+        
+        yield return new WaitForSeconds(holdTime);
+        
+        while (counter > 0)
+        {
+            repeatEvent.Invoke();
+            yield return new WaitForSeconds(repeatHoldTime);
+            counter--;
+        }
+        
+        yield return new WaitForSeconds(holdTime);
+        endEvent.Invoke();
+    }
+}
