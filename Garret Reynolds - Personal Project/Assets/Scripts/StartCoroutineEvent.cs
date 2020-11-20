2c929ec3bcef5fa6c419faa6e3bf95562e67d177
Garret <10901561@my.uvu.edu>
2020-11-20 16:39:09 -0700

    Start Of HW Assignment

---
+++ Garret Reynolds - Personal Project/Assets/Scripts/StartCoroutineEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StartCoroutineEvent : MonoBehaviour
+{
+    public UnityEvent startEvent, repeatEvent, endEvent;
+    public float holdTime = 1f, repeatHoldTime;
+    public int counter = 3;
+
+    public void RunCoroutine()
+    {
+        StartCoroutine(Coroutine());
+    }
+
+    private IEnumerator Coroutine()
+    {
+        startEvent.Invoke();
+        
+        yield return  new WaitForSeconds(holdTime);
+
+        while (counter > 0)
+        {
+            repeatEvent.Invoke();
+            yield return new WaitForSeconds(repeatHoldTime);
+            counter--;
+        }
+    }
+}
