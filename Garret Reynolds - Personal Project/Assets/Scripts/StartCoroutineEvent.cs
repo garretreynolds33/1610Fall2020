3719aa003ccbd3ab0c1ae8fd0277ef0e82e547f7
Garret <10901561@my.uvu.edu>
2020-11-23 20:02:38 -0700

    Finally Figured out the Counter
    It looks weird and I'm not sure I did it right, but it works!

--- Garret Reynolds - Personal Project/Assets/Scripts/StartCoroutineEvent.cs
+++ Garret Reynolds - Personal Project/Assets/Scripts/StartCoroutineEvent.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class StartCoroutineEvent : MonoBehaviour
 {
     public UnityEvent startEvent, repeatEvent, endEvent;
-    public float holdTime = 1f, repeatHoldTime;
+    public float holdTime = 1f, repeatHoldTime = 1f;
     public int counter = 3;
+    public Text text;
 
-    public void RunCoroutine()
+
+    public void Start()
     {
         StartCoroutine(Coroutine());
     }
+    
 
     private IEnumerator Coroutine()
     {
@@ -25,6 +30,14 @@ public class StartCoroutineEvent : MonoBehaviour
             repeatEvent.Invoke();
             yield return new WaitForSeconds(repeatHoldTime);
             counter--;
+            text.text = counter.ToString();
+            if (counter.Equals(0))
+            {
+                text.text = "Go!";
+            }
         }
+        yield return new WaitForSeconds(holdTime);
+        endEvent.Invoke();
+
     }
 }
