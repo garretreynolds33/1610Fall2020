4d31cbf5ab9e5788f5f3c37154537367a76bfd26
Garret <10901561@my.uvu.edu>
2020-11-30 12:49:24 -0700

    HW Nov 30th

---
+++ Garret Reynolds - Personal Project/Assets/Scripts/ObstacleMover.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMover : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    
+    void Update()
+    {
+        transform.Translate(Vector3.left * (Time.deltaTime * moveSpeed));
+    }
+}
--- Garret Reynolds - Personal Project/Assets/Scripts/SpawnManager.cs
+++ Garret Reynolds - Personal Project/Assets/Scripts/SpawnManager.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LaserMover : MonoBehaviour
+public class SpawnManager : MonoBehaviour
 {
-    public float speed = 50f;
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +13,6 @@ public class LaserMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * (Time.deltaTime * speed));
+        
     }
 }
