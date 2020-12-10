2fd82df4dbbef44bc22a40521b38bbd7f37403d3
Garret <10901561@my.uvu.edu>
2020-12-10 14:44:07 -0700

    HW Dec 10th

---
+++ Garret Reynolds - Personal Project/Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    private float rotateSpeed = 15f;
+    
+    void Update()
+    {
+        transform.Rotate(0,rotateSpeed* Time.deltaTime,0);
+    }
+}
